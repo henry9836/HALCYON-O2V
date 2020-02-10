c7ccd7b7faf6daf42b95816034bda1e49405eab4
Vaughan Webb <vaughanw2012@gmail.com>
2020-02-10 23:03:56 +1300

    mouse selection

--- Assets/Scripts/camera/selection.cs
+++ Assets/Scripts/camera/selection.cs
@@ -12,6 +12,8 @@ public class selection : MonoBehaviour
     public Vector2 endui;
     public GameObject selectionUI;
 
+    public List<Vector3> points;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0) == true)
@@ -25,49 +27,62 @@ public class selection : MonoBehaviour
         {
             endui = Input.mousePosition;
 
-            selectionUI.gameObject.GetComponent<RectTransform>().localPosition = startui;
+            selectionUI.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(startui.x / Screen.width, startui.y / Screen.height);
+            selectionUI.gameObject.GetComponent<RectTransform>().anchorMax = selectionUI.gameObject.GetComponent<RectTransform>().anchorMin;
 
 
             if (startui.x > endui.x)
             {
                 if (startui.y > endui.y)
                 {
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(0.0f, 0.0f);
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(0.0f, 0.0f);
+                    selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Abs(endui.x - startui.x), Mathf.Abs(endui.y - startui.y));
+                    selectionUI.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-(selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.x / 2), -(selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2));
+
                 }
                 else
                 {
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(0.0f, 0.0f);
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(0.0f, 0.0f);
+                    selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Abs(endui.x - startui.x), Mathf.Abs(endui.y - startui.y));
+                    selectionUI.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-(selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.x / 2), (selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2));
                 }
             }
             else
             {
                 if (startui.y > endui.y)
                 {
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(0.0f, 0.0f);
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(0.0f, 0.0f);
+                    selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Abs(endui.x - startui.x), Mathf.Abs(endui.y - startui.y));
+                    selectionUI.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2((selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.x / 2), -(selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2));
+
                 }
                 else
                 {
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMin = new Vector2(0.0f, 0.0f);
-                    selectionUI.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(0.0f, 0.0f);
+                    selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Abs(endui.x - startui.x), Mathf.Abs(endui.y - startui.y));
+                    selectionUI.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2((selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.x / 2), (selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2));
                 }
-
             }
 
+            end = this.gameObject.GetComponent<mousepick>().getMousePos();
+
+            points.Clear();
+            points.Add(start);
+            //points.Add(); other 2 corners after raycasting
+            points.Add(end);
 
         }
-
-
 
         if (Input.GetMouseButtonUp(0) == true)
         {
-            end = this.gameObject.GetComponent<mousepick>().getMousePos();
             startui = Vector3.zero;
             endui = Vector3.zero;
+            selectionUI.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(0.0f, 0.0f);
             Debug.DrawLine(start, end, Color.red, 10.0f);
             pressed = false;
         }
+    }
+
+
+    void selectunits()
+    { 
+    
+    
     }
 }
