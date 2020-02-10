04246543efcee5fc5a0b0a32ed9b12179c5e81cb
Vaughan Webb <vaughanw2012@gmail.com>
2020-02-10 16:16:18 +1300

    keyt based movment

---
+++ Assets/Scripts/camera/arrowmove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowmove : MonoBehaviour
+{
+    public Vector3 newPos;
+    void Update()
+    {
+        newPos = Vector3.zero;
+        newPos += Input.GetAxisRaw("Vertical") * Vector3.up;
+        newPos += Input.GetAxisRaw("Horizontal") * new Vector3(1.0f, 0.0f, -1.0f);
+        this.gameObject.transform.position += newPos;
+    }
+}
