e760f2ed23dc3e79241204a1e03e96c09e70ecee
Vaughan Webb <vaughanw2012@gmail.com>
2020-02-12 09:08:57 +1300

    mosue camera movment

--- Assets/Scripts/camera/arrowmove.cs
+++ Assets/Scripts/camera/arrowmove.cs
@@ -11,12 +11,30 @@ public class arrowmove : MonoBehaviour
     public float speed = 1.0f;
     void Update()
     {
-
         if (movable == true)
         {
             newPos = Vector3.zero;
             newPos += Input.GetAxisRaw("Vertical") * Vector3.up;
             newPos += Input.GetAxisRaw("Horizontal") * new Vector3(1.0f, 0.0f, -1.0f);
+
+
+            if (Input.mousePosition.y >= Screen.height)
+            {
+                newPos += Vector3.up;
+            }
+            if (Input.mousePosition.x >= Screen.width)
+            {
+                newPos += new Vector3(1.0f, 0.0f, -1.0f);
+            }
+            if (Input.mousePosition.y <= 1.0f)
+            {
+                newPos -= Vector3.up;
+            }
+            if (Input.mousePosition.x <= 1.0f)
+            {
+                newPos -= new Vector3(1.0f, 0.0f, -1.0f);
+            }
+
             this.gameObject.transform.position += newPos * speed;
 
             float wheel = Input.GetAxis("Mouse ScrollWheel");
@@ -29,8 +47,9 @@ public class arrowmove : MonoBehaviour
                 zoom -= wheel * 2.0f;
             }
             this.gameObject.GetComponent<Camera>().orthographicSize = zoom;
+
+
+
         }
-
-
     }
 }
