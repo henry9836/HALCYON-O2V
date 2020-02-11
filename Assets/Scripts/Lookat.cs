bc52a5a988554be00a95268de2b0ffe239bd3828
Vaughan Webb <vaughanw2012@gmail.com>
2020-02-11 17:06:48 +1300

    player selection finished

--- Assets/Scripts/Lookat.cs
+++ Assets/Scripts/Lookat.cs
@@ -6,6 +6,11 @@ public class Lookat : MonoBehaviour
 {
     void Update()
     {
-        this.transform.eulerAngles = new Vector3(GameObject.Find("Main Camera").transform.eulerAngles.x, GameObject.Find("Main Camera").transform.eulerAngles.y, GameObject.Find("Main Camera").transform.eulerAngles.z); 
+        looktowards();
+    }
+
+    public void looktowards()
+    {
+        this.transform.eulerAngles = new Vector3(GameObject.Find("Main Camera").transform.eulerAngles.x, GameObject.Find("Main Camera").transform.eulerAngles.y, GameObject.Find("Main Camera").transform.eulerAngles.z);
     }
 }
