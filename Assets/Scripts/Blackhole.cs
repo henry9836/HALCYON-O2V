fcece69d654ddcfa6015ba5be87f411d52eb86e6
Vaughan Webb <vaughanw2012@gmail.com>
2020-02-24 09:04:58 +1300

    made luncher prefab

---
+++ Assets/Scripts/Blackhole.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackhole : MonoBehaviour
+{
+    public float radius = 5.0f;
+
+    public void FixedUpdate()
+    {
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(-10.0f, this.transform.position, radius, 3.0f);
+            }
+        }
+    }
+}
