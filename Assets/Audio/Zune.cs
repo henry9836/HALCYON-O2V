a195d916f105558551d45827c47b8d0fb879ab39
Henry Oliver <henry983615@gmail.com>
2020-02-28 10:02:26 +1300

    Music

---
+++ Assets/Audio/Zune.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zune : MonoBehaviour
+{
+    public List<AudioClip> clips = new List<AudioClip>();
+    AudioSource aSource;
+
+    private void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!aSource.isPlaying)
+        {
+            aSource.clip = clips[Random.Range(0, clips.Count)];
+            aSource.Play();
+        }
+    }
+}
