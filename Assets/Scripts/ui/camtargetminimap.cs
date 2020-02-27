6a98c8233463bdcfe62b7c209d2b7577d21ee08d
Vaughan Webb <vaughanw2012@gmail.com>
2020-02-27 20:28:01 +1300

    ui minimap done

--- Assets/Scripts/ui/camtargetminimap.cs
+++ Assets/Scripts/ui/camtargetminimap.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class camtargetminimap : MonoBehaviour
 {
+    private GameObject cam;
+    public LayerMask gridmask;
 
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").gameObject;
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, Mathf.Infinity, gridmask))
+        {
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * hit.distance, Color.yellow);
+        }
+
+        this.gameObject.transform.position = new Vector3(hit.point.x, hit.point.y + 300.0f, hit.point.z);
+    }
 }
